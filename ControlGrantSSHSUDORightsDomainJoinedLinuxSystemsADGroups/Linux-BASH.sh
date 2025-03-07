f262fc4a2714aabd440ab476490516fd4094e327
Darien Hawkins <darien-hawkins@hotmail.com>
2025-03-07 13:02:58 -0500

    Add files via upload

---
+++ ControlGrantSSHSUDORightsDomainJoinedLinuxSystemsADGroups/Linux-BASH.sh
@@ -0,0 +1,12 @@
+sudo systemctl stop sssd
+sss_cache -E
+rm -f /var/lib/sss/{db,mc}/*
+sudo systemctl start sssd
+
+sudo nano /etc/ssh/sshd_config.d/[filename].conf
+sudo visudo -f /etc/sudoers.d/[filename]
+
+sudo systemctl stop sssd
+sss_cache -E
+rm -f /var/lib/sss/{db,mc}/*
+sudo systemctl start sssdd
